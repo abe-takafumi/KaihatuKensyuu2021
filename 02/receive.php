1e672c059fb07609c00fb7c50af7adb4ff299db5
abe-takafumi <abe-takafumi@webrage.jp>
2021-08-03 17:57:56 +0900

    ・フォーム部品をforms.htmlに配置しました。 ・recieve.phpによってforms.htmlのデータを受け取る処理を行いました。 ・tableを用いてforms.htmlの修正を行いました。

---
+++ 02/receive.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>第二回課題、フォーム部品練習</title>
+    </head>
+    <body>
+        <?php
+            echo "これは". $_GET['name'] . " さんのアンケート結果です<br/>";
+            echo "このアンケート結果は " . $_GET['post'] . " へ送信されます<br/>";
+            echo $_GET['name'] . "さんが一番好きな食べ物は「 " . $_GET['radio'] . "」です。<br/>";
+            echo "食べたい季節は " . $_GET['kisetu'] . " だそうです。<br/>";
+            echo "好きなトッピングは " . $_GET['hobby'][0] . ", " . $_GET['hobby'][1] . ", " . $_GET['hobby'][2] .  " ですね。<br/>";
+            echo $_GET['name'] . "さんからのご意見です。<br/> 「" . $_GET['kansou']."」" ;
+        ?>
+    </body>
+</html>
